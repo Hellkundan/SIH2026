846a42a559ec49da2b5456df85396b22a5e27a08
Abharayendu <abharayendukumarghosh@gmail.com>
2026-09-08 20:26:08 +0530

    updated main.py

--- src/verification_hub/app/main.py
+++ src/verification_hub/app/main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.verification_hub.app.api.routes import router
+from src.verification_hub.appcore.config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+    description="verification hub",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
 @app.get("/")
 async def root():
     return {
-        "service" : "verification_hub",
-        "status" : "RUNNING",
+        "service": settings.APP_NAME,
+        "status": "RUNNING",
     }
+
 @app.get("/health")
 async def health():
-    return {"status" : "UP"}
+    return {"status": "UP"}
